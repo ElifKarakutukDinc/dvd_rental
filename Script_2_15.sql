ca480171f3fb0059b97505838517701de4af71c8
Elif Karakutuk Dinc <elifkaraktk@gmail.com>
2019-10-30 11:23:24 +0100

    Script_2_15,Script_2_22

--- Script_2_15.sql
+++ Script_2_15.sql
@@ -4,6 +4,6 @@ select a.actor_id, a.first_name, a.last_name, count(distinct fc.category_id)
 from actor as a
 left join film_actor as fa on (a.actor_id=fa.actor_id)
 left join film_category as fc on (fa.film_id=fc.film_id)
-group by a.actor_id
+group by a.actor_id, a.first_name, a.last_name
 order by count(fc.category_id) desc
 limit 5;
--- Script_2_22.sql
+++ Script_2_22.sql
@@ -1,7 +1,15 @@
 --Customer list who changed their addresses after last successful payment?
 
-select c.customer_id
+with last_payment_date as (
+select c.customer_id, max(payment_date) as max_payment_date
 from customer as c 
 left join address as ad on (c.address_id=ad.address_id)
 left join payment as p on (c.customer_id=p.customer_id)
-where payment_date < ad.last_update;
+group by c.customer_id)
+
+select lpd.*, ad.last_update
+from last_payment_date as lpd
+left join customer as c on (lpd.customer_id = c.customer_id)
+left join address as ad on (c.address_id=ad.address_id)
+left join payment as p on (c.customer_id=p.customer_id)
+where max_payment_date < ad.last_update
